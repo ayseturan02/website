43d35850f3f721c6c4d42118f835dafdc91f12d6
ayşeturan02 <ayse0025turan@gmail.com>
2024-02-18 22:33:45 +0300

    required CRUD operations

---
+++ app/Http/Controllers/Back/CommentsController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Back;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comments;
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        return view("back.pages.comments.index");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $comments = Comments::find($id);
+        if (isset($comments)) {
+            $comments->delete();
+        }
+        return redirect()->route("admin.yorum.index");
+    }
+}
